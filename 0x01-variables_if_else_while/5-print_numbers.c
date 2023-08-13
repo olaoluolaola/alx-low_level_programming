377d806fb388b9a2dcad21b3dc042d6629664f48
olaoluolaola <olaoluolaola@gmail.com>
2023-08-13 10:03:58 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the alphabet in lowercase and uppercae
+ * main - program that prints the alphabet in lowercase and upper case
  *
  * Return: Always 0.
  */
