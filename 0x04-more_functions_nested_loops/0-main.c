787fd6d4f5c9c7ecf52fb1be7d41f2c6f1a496ef
olaoluolaola <olaoluolaola@gmail.com>
2023-08-23 10:05:56 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        int i;
+	char c;
 
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
